6a36fd4f5a79df36c269c303afaf52ff3a6b54ff
Philip Cook <philip.cook12@gmail.com>
2013-05-08 02:20:18 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstutapp::Application.config.secret_token = '5fb01076204c23c30f26d806775c837d03c5773441ef4592ce2b5265e5673c5b773417a2f78e7dae6d23e3876f0116119e81c9d64d86710b721ed584a66f7641'
